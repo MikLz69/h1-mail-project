110d9ded28903063c6d66b4cde9c77d3f9b00f50
MikLz69 <mtkongsted@gmail.com>
2022-04-05 03:07:17 -0700

    recreated project

--- Program.cs
+++ Program.cs
@@ -4,25 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Mail_projekt
+namespace h1_mail_program
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            const string[] commands = new string[]
-            {
-                "mail info: prints user information",
-                "mail inbox: prints mail inbox",
-                "mail view <mail>: view a specific mail",
-                "mail send: create and send your own mail"
-            };
-            string command = Console.ReadLine()
-            if command.ToLower() = "mail help" {
-                for (int i = 0; i < commands.Length; i++)
-                    Console.WriteLine(commands[i] + "\n")
-            }
-            
         }
     }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Mail projekt")]
+[assembly: AssemblyTitle("h1 mail program")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Mail projekt")]
+[assembly: AssemblyProduct("h1 mail program")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e9de8f22-2e61-4db6-b732-ec9e7a046a5a")]
+[assembly: Guid("9eab41f9-3ad9-4046-abee-dee12d7e4943")]
 
 // Version information for an assembly consists of the following four values:
 //

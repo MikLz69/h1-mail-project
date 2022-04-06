d376c4c43fce8c52f595666a5d0a5fdef1dcb1f5
MikLz69 <mtkongsted@gmail.com>
2022-04-06 23:37:41 +0200

    started on translating to csharp

--- Program.cs
+++ Program.cs
@@ -1,34 +1,55 @@
 ﻿using System;
-using System.Net;
+using System.Text.Json;
 
 namespace Program{
-    class ServerClient {
-        private readonly HttpClient httpClient;
+
+    public class Mail {
+        public string? title {get; set; }
+        public string? content {get; set; }
+        public int? id { get; set; }
+        public string? sender { get; set; }
+    }
+
+    public class ServerClient {
+        private readonly HttpClient client;
         public ServerClient() {
-            httpClient = new HttpClient();
-            System.Net.ServicePointManager.SecurityProtocol |= ee.Tls12 |e SecurityProtocolType.Tls11 | SecurityProtocolType.Tls;e
-        }eee+303eee
-    } e
+            client = new HttpClient();
+        }
+
+        public async Task<Mail> Read() {
+            var json = await client.GetStringAsync("http://localhost:8000/read?user=Mikkel&number=1") ;
+            var mail = JsonSerializer.Deserialize<Mail>(json);
+            if (mail == null)
+                throw new Exception();
+            return mail;
+        }
+    }
     public class Program {
+        public static ServerClient? sc;
         public static void Main(string[] args) {
+            sc = new ServerClient();
             var program = new Program();
             string user = "Mikkel";
             var commands = new string[]
             {
                 "mail info: prints user information",
                 "mail inbox: prints mail inbox",
-                "mail read <mail>: view a specific mail",
+                "mail read <mail number>: view a specific mail",
                 "mail send: create and send your own mail"
             };
             var inbox = new List<string>() {
             };
             Console.WriteLine("Welcome to the mail, type `mail help` for available commands");
             program.runProgram(commands, inbox, user);
-            
+            for (;;);
         }
-            public void runProgram(string[] commands, List<string> inbox, string user) {
+            public async void runProgram(string[] commands, List<string> inbox, string user) {
+            if (sc == null)
+                throw new NullReferenceException();
+            Console.WriteLine(await sc.Read());
             var command = Console.ReadLine();
             command = command?.ToLower();
+
 
             if (command == "mail help") {
                 for (int i = 0; i < commands.Length; i++)
--- README.md
+++ README.md
@@ -19,3 +19,9 @@ curl -X GET localhost/inbox
 ```
 curl -X POST localhost/send
 ```
+
+## Resources
+
+- [HttpClient - Zetcode.com](https://zetcode.com/csharp/httpclient/)
+- [Asynchronous programming - MS Docs](https://docs.microsoft.com/en-us/dotnet/csharp/async)
+- [How to serialize and deserialize JSON - MS Docs](https://docs.microsoft.com/en-us/dotnet/standard/serialization/system-text-json-how-to?pivots=dotnet-6-0)
